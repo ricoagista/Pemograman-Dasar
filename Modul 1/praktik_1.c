0692282268f2190364fdd5f7e8c144b384dc8ef8
Rico Shandika Jovial Agista <97493070+ricoagista@users.noreply.github.com>
2024-09-25 00:15:10 +0700

    tambah praktik

---
+++ Modul 1/praktik_1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    int p = 10;
+    float l = 0.656;
+    char a = 'a';
+
+    printf("Nilai P: %d\n", p);
+
+    printf("Nilai l: %.2f\n", l);
+
+    printf("Karakter a: %c\n", a);
+
+    char b[5] = "abcde";
+    printf("String b: %s\n", b);
+
+    return 0;
+}
