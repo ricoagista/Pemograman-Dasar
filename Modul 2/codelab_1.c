4f40369998cfdb6959c290d6b1bc04ec6a2c9259
Rico Shandika Jovial Agista <97493070+ricoagista@users.noreply.github.com>
2024-10-09 12:48:11 +0700

    modul 2

---
+++ Modul 2/codelab_1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    const int BATAS_KALORI_HARIAN = 500; // Sesuaikan batas kalori harian sesuai kebutuhan
+    int kalori_per_makanan, total_kalori = 0;
+
+    // Pengenalan program
+    printf("==== Program Penghitung Kalori Harian ====\n");
+    printf("Program ini akan membantu Anda menghitung total kalori yang dikonsumsi dalam sehari.\n\n");
+
+    // Mengambil input dari user
+    printf("Masukkan kalori makanan pertama: ");
+    scanf("%d", &kalori_per_makanan);
+    total_kalori += kalori_per_makanan;
+
+    printf("Masukkan kalori makanan kedua: ");
+    scanf("%d", &kalori_per_makanan);
+    total_kalori += kalori_per_makanan;
+
+    printf("Masukkan kalori makanan ketiga: ");
+    scanf("%d", &kalori_per_makanan);
+    total_kalori += kalori_per_makanan;
+
+    // Menampilkan pesan apakah total kalori melebihi batas kalori harian atau tidak
+    if (total_kalori > BATAS_KALORI_HARIAN) {
+        printf("\nTotal kalori Anda %d melebihi batas kalori harian.\n", total_kalori, BATAS_KALORI_HARIAN);
+    } else {
+        printf("\nTotal kalori Anda %d tidak melebihi batas kalori harian.\n", total_kalori, BATAS_KALORI_HARIAN);
+    }
+
+    // Penutupan program
+    printf("\n===Code Execution Succesful ===\n");
+
+    return 0;
+}
