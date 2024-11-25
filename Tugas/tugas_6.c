6e721002216e55c88fd10822a5ecbb226bb32722
Rico Shandika Jovial Agista <97493070+ricoagista@users.noreply.github.com>
2024-11-25 17:11:41 +0700

    modul

--- Tugas/tugas_6.c
+++ Tugas/tugas_6.c
@@ -1,39 +1,53 @@
 #include <stdio.h>
 
+void tampilkanGenap(int arr[][4], int baris, int kolom) {
+    for (int i = 0; i < baris; i++) {
+        for (int j = 0; j < kolom; j++) {
+            if (arr[i][j] % 2 == 0) {
+                printf("%d ", arr[i][j]);
+            }
+        }
+    }
+    printf("\n");
+}
+
 int main() {
-    char kategori[20];
-    double penghasilan, pajak, gajiBersih;
+    int x[3][4] = {
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 10, 11}
+    };
 
-    printf("Masukkan kategori (pebisnis/pekerja): ");
-    scanf("%s", kategori);
+    // Langkah 2: Tampilkan bilangan genap
+    printf("Bilangan genap:\n");
+    tampilkanGenap(x, 3, 4);
 
-    printf("Masukkan penghasilan: ");
-    scanf("%lf", &penghasilan);
+    // Langkah 3: Buat array baru untuk bilangan genap dan ganjil
+    int genap[10], ganjil[10]; // Asumsikan maksimal 10 bilangan genap/ganjil
+    int idxGenap = 0, idxGanjil = 0;
 
-    if (strcmp(kategori, "pebisnis") == 0) {
-        if (penghasilan <= 3500000) {
-            pajak = 0.25;
-        } else {
-            pajak = 0.2;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (x[i][j] % 2 == 0) {
+                genap[idxGenap++] = x[i][j];
+            } else {
+                ganjil[idxGanjil++] = x[i][j];
+            }
         }
-    } else if (strcmp(kategori, "pekerja") == 0) {
-        if (penghasilan <= 2500000) {
-            pajak = 0.15;
-        } else if (penghasilan <= 2000000) {
-            pajak = 0.1;
-        } else if (penghasilan <= 3000000) {
-            pajak = 0.15;
-        } else {
-            pajak = 0.2;
-        }
-    } else {
-        printf("Kategori salah.\n");
-        return 1;
     }
 
-    gajiBersih = penghasilan - (penghasilan * pajak);
+    // Tampilkan array baru
+    printf("Bilangan genap: ");
+    for (int i = 0; i < idxGenap; i++) {
+        printf("%d ", genap[i]);
+    }
+    printf("\n");
 
-    printf("Gaji bersih: %.2lf\n", gajiBersih);
+    printf("Bilangan ganjil: ");
+    for (int i = 0; i < idxGanjil; i++) {
+        printf("%d ", ganjil[i]);
+    }
+    printf("\n");
 
     return 0;
 }
